f19825cf24f7a6929eb1976ee17e7fade8d61b03
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-04-23 22:16:58 -0400

    added windows script

--- Releases/generate-zips.sh
+++ Releases/generate-zips.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ../macos
+cd ../macos || exit
 zip -r ../Releases/MacOS.zip nvfPostProcessor setup-postprocessor-macos.sh nvfPostprocessor.app
-cd -
+cd - || exit
 
-cd ../windows
+cd ../windows || exit
 zip -r ../Releases/Windows.zip nvfPostprocessor.exe setup-postprocessor-windows.bat create_shortcut.vbs
-cd -
+cd - || exit
